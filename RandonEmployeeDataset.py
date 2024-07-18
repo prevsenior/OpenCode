6d122ecd0015c465a17560f07e51e12e596f5573
Washington Ribeiro <contato@prevsenior.com.br>
2024-07-18 07:12:39 -0700

    Ajuste no codigo de geracao de nome randomico

--- RandonEmployeeDataset.py
+++ RandonEmployeeDataset.py
@@ -5,26 +5,33 @@
 import random
 import string
 import os
+import zipfile
 
 def generate_employee_file(filename):
-  with open(filename, 'w') as f:
-    f.write('name,birthdate,sex,phonenumber,email,id,hiredate,status\n')
-    for _ in range(10):
-      name = ''.join(random.choices(string.ascii_lowercase, k=random.randint(5, 10)))
-      birthdate = str(random.randint(1950, 2000)) + '-' + str(random.randint(1, 12)) + '-' + str(random.randint(1, 28))
-      sex = random.choice(['M', 'F'])
-      phonenumber = str(random.randint(100000000, 999999999))
-      email = name + '@' + ''.join(random.choices(string.ascii_lowercase, k=random.randint(5, 10))) + '.com'
-      id = str(random.randint(100000, 999999))
-      hiredate = str(random.randint(2000, 2022)) + '-' + str(random.randint(1, 12)) + '-' + str(random.randint(1, 28))
-      status = random.choice(['A', 'I'])
-      f.write(','.join([name, birthdate, sex, phonenumber, email, id, hiredate, status]) + '\n')
+    with open(filename, 'w') as f:
+        f.write('name,birthdate,sex,phonenumber,email,id,hiredate,status\n')
+        for _ in range(10):
+            name = ''.join(random.choices(string.ascii_lowercase, k=random.randint(5, 10)))
+            birthdate = f"{random.randint(1950, 2000)}-{random.randint(1, 12):02d}-{random.randint(1, 28):02d}"
+            sex = random.choice(['M', 'F'])
+            phonenumber = str(random.randint(100000000, 999999999))
+            email = f"{name}@{''.join(random.choices(string.ascii_lowercase, k=random.randint(5, 10)))}.com"
+            emp_id = str(random.randint(100000, 999999))
+            hiredate = f"{random.randint(2000, 2022)}-{random.randint(1, 12):02d}-{random.randint(1, 28):02d}"
+            status = random.choice(['A', 'I'])
+            f.write(','.join([name, birthdate, sex, phonenumber, email, emp_id, hiredate, status]) + '\n')
 
+# Gerar os arquivos CSV
 for i in range(120):
-  filename = 'employee_data_{}.csv'.format(i)
-  generate_employee_file(filename)
+    filename = f'employee_data_{i}.csv'
+    generate_employee_file(filename)
 
-!zip -r employee_data.zip *.csv
-!rm *.csv
-!echo "Download the employee_data.zip file by clicking on the link below."
-!echo "<a href='employee_data.zip' download>Download</a>"
+# Compactar os arquivos CSV em um arquivo ZIP
+with zipfile.ZipFile('employee_data.zip', 'w') as zipf:
+    for i in range(120):
+        filename = f'employee_data_{i}.csv'
+        zipf.write(filename)
+        os.remove(filename)
+
+print("Arquivo 'employee_data.zip' criado com sucesso.")
+
